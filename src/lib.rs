cd024ce493845ede1d2409a3f2f2409709e6a0f8
Matheus Santos <39751586+melizimar@users.noreply.github.com>
2024-06-05 08:47:45 -0300

    Tornando o codigo funcional
    Concluindo o tutorial da documentação

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,26 @@ impl Config {
     }
 }
 
-pub fn run (config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    return results;
 }
 
 #[cfg(test)]
